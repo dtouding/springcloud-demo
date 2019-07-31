16a973d162d952e8f45892a368fe0cbc2bcdc0be
dtouding <dtouding@aliyun.com>
2019-07-31 22:23:05 +0800

    负载均衡切换

---
+++ ribbon-consumer/src/main/java/com/dtouding/ribbonconsumer/ConfigBean.java
@@ -0,0 +1,30 @@
+package com.dtouding.ribbonconsumer;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * ribbon默认负载均衡策略是RoundRobinRule-轮询策略
+     * 若想改为随机策略，可注入RandomRule到spring容器
+     * @return
+     */
+    @Bean
+    public IRule rule() {
+        return new RandomRule();
+    }
+
+}
--- ribbon-consumer/src/main/java/com/dtouding/ribbonconsumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/dtouding/ribbonconsumer/RibbonConsumerApplication.java
@@ -1,24 +1,18 @@
 package com.dtouding.ribbonconsumer;
 
+import com.dtouding.rule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "HELLO-SERVICE", configuration = MyRule.class)
 public class RibbonConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonConsumerApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
 }
