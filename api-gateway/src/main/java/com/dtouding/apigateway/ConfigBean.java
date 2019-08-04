251e143930fe023f96baae0f6d0807c876a3dc5f
dtouding <dtouding@aliyun.com>
2019-08-04 12:14:41 +0800

    自定义服务与路由的映射规则

--- api-gateway/src/main/java/com/dtouding/apigateway/ConfigBean.java
+++ api-gateway/src/main/java/com/dtouding/apigateway/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.dtouding.apigateway;
 
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +10,13 @@ public class ConfigBean {
     @Bean
     public AccessFilter accessFilter() {
         return new AccessFilter();
+    }
+
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper(
+                "(?<name>^.+$)",
+                "dtouding/${name}"
+        );
     }
 }
