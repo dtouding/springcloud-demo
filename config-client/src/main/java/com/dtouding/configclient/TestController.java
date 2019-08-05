28ce5813e183d26287280020fe4b8e1b0db12e37
dtouding <dtouding@aliyun.com>
2019-08-05 22:05:41 +0800

    修改配置测试

--- config-client/src/main/java/com/dtouding/configclient/TestController.java
+++ config-client/src/main/java/com/dtouding/configclient/TestController.java
@@ -1,6 +1,8 @@
 package com.dtouding.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,13 @@ public class TestController {
     @GetMapping("/from")
     public String from() {
         return from;
+    }
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/from2")
+    public String from2() {
+        return env.getProperty("from", "default");
     }
 }
